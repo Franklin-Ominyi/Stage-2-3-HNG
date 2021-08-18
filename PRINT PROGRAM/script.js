22e024c52c649d340fe95f0248fe68da9f5b5b90
Franklin-Ominyi <franklinchibuikeominyi@gmail.com>
2021-08-18 13:31:36 -0700

    first commit

---
+++ PRINT PROGRAM/script.js
@@ -0,0 +1,2 @@
+document.write('Fullname: Alegu Franklin Ominyi, Display Name: Chitech');
+console.log("Fullname: Alegu Franklin Ominyi, Display Name: Chitech")
---
+++ RESUME/js/script.js
@@ -0,0 +1,26 @@
+/*smooth scroll effect*/
+$(function() {
+ $("a.smooth-scroll").click(function(event){
+  event.preventDefault();
+
+  //get/return id like #about, #home, etc
+  var section = $(this).attr("href");
+
+  $('html, body').animate({
+   scrollTop: $(section).offset().top
+  }, 2000);
+ });
+});
+
+/*Hide navbar on scroll*/
+let prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+ let currentScrollPos = window.pageYOffset;
+ if(prevScrollPos > currentScrollPos) {
+  document.querySelector('.navbar').style.top = '0';
+ } else {
+  document.querySelector('.navbar').style.top = '-60px';
+ }
+ // console.log(prevScrollPos, currentScrollPos)
+ prevScrollPos = currentScrollPos;
+}
